8063d3ec5b9bf60b8e192ae4ea98a8d02868ac89
devan4264 <157516863+devan4264@users.noreply.github.com>
2024-02-01 11:40:53 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+//$ jquery land
+$(document).ready(function(){
+  console.log("Top: " + $("img").position().top + " Left: " + $("img").position().left);
+  
+  $("p").position();
+ 
+  //make image draggable
+  
+  $("#dogememe").draggable({
+    stop: function () {
+      console.log("stopping???");
+
+      // if( img is far right side  then ) user wins.
+
+      let dogeLeft = $("img").position().left;
+
+      if (dogeLeft > 1200) {
+        console.log("we above 1200!", dogeLeft);
+
+        $("h1").text("Am full now, much happy!");
+
+      } else {
+        console.log("we NOT above 1200");
+      }
+    },
+  });
+});
